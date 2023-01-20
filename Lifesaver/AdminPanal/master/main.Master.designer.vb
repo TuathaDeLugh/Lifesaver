907cadcaa9fd3198ddbaf351c11a4c17c056eede
Umang Sailor <ursailor@gmail.com>
2023-01-20 10:01:24 +0530

    das

--- Lifesaver/AdminPanal/master/main.Master.designer.vb
+++ Lifesaver/AdminPanal/master/main.Master.designer.vb
@@ -113,6 +113,33 @@ Partial Public Class main
     Protected WithEvents HyperLink11 As Global.System.Web.UI.WebControls.HyperLink
 
     '''<summary>
+    '''HyperLink14 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents HyperLink14 As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''HyperLink15 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents HyperLink15 As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
+    '''HyperLink16 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents HyperLink16 As Global.System.Web.UI.WebControls.HyperLink
+
+    '''<summary>
     '''HyperLink4 control.
     '''</summary>
     '''<remarks>
